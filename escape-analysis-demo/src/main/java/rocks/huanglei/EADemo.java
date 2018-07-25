01af95e03e5860b61fc4d0dbcb4f039707cba5c1
葛努 <gnu.hl@antfin.com>
2018-07-25 11:23:49 +0800

    [fix] deopt

--- escape-analysis-demo/src/main/java/rocks/huanglei/EADemo.java
+++ escape-analysis-demo/src/main/java/rocks/huanglei/EADemo.java
@@ -1,12 +1,17 @@
 package rocks.huanglei;
 
+
 public class EADemo {
     public static final long LEN_IN_GO_DEMO   = 10_000_000_000L;
     public static final int  LEN_IN_THIS_DEMO = (int)LEN_IN_GO_DEMO / 10;
 
+    static {
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         //for (int i = 0; i < 100; i++) {
-        long[] c = new long[LEN_IN_THIS_DEMO];
+        long[] c = new long[LEN_IN_THIS_DEMO]; // ~7.4GB
         System.out.println(c.length);
         //}
     }
