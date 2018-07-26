1bd989b1b97d7ac94f5e8c7ecf012bb9f5c27729
Lei <mrsatangel@gmail.com>
2018-07-26 16:38:51 +0800

    fix build script for native netty demo

--- native-netty/build.sh
+++ native-netty/build.sh
@@ -4,4 +4,4 @@
 
 mvn install:install-file -Dfile=${GRAALVM_HOME}/jre/lib/svm/builder/svm.jar -DgroupId=com.oracle.substratevm -DartifactId=svm -Dversion=GraalVM-1.0.0-rc1 -Dpackaging=jar
 mvn clean package
-${GRAALVM_HOME}/bin/native-image -jar $(dirname $0)/native-netty-1.0-SNAPSHOT.jar -H:ReflectionConfigurationResources=reflection_config.json
+${GRAALVM_HOME}/bin/native-image -jar $(dirname $0)/target/native-netty-1.0-SNAPSHOT.jar -H:ReflectionConfigurationResources=reflection_config.json
