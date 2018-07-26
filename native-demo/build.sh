6ca3f244eec0f41a9f21604cd19c6c0675323d1d
Lei <mrsatangel@gmail.com>
2018-07-26 16:24:41 +0800

    fix build script

--- native-demo/build.sh
+++ native-demo/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 [[ -z "${GRAALVM_HOME}" ]] && { echo "Please install GraalVM and set env GRAALVM_HOME"  1>&2 ; exit 1; }
+mvn clean compile
 cd $(dirname $0)/target
 ${GRAALVM_HOME}/bin/native-image -cp classes rocks.huanglei.TopTenSingleFile
